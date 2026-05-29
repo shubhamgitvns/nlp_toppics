968cb26ecb6bba8651da264216c8d5fa8ef98979
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-05-29 18:32:32 +0530

    tokenize data from using librery

--- tokenization.py
+++ tokenization.py
@@ -1,11 +1,34 @@
-import sys
+import re
 import nltk
-import sys
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+from nltk.tokenize import word_tokenize
 
-print(sys.executable)
+data ="random 100 words peragaraph which include punchuation symbolsThe old," \
+" dusty bookstore—located on 5th Avenue—was a hidden treasure trove; it smelled of aged paper," \
+" leather, and vanilla. Look at this! exclaimed Sarah, pointing eagerly toward a leather-bound " \
+"journal. The cover was worn, yet beautiful; it had a strange, golden crest engraved in the center." \
+" She asked the clerk, How much for this item? He smiled, stroked his gray beard, and whispered," \
+" For you? It's free. Sarah gasped. Was this a trick? She opened the first page, finding a handwritten note: Adventure awaits"
 
-# if nltk downloding issue try thi
-# C:\Users\shubham\.local\bin\python3.14.exe -m pip install nltk --break-system-packages
-# python -m pip install nltk --break-system-packages
+# print len of data
+print(len(data))
 
-print("nltk download")
+# covert data in lowercase
+data = data.lower()
+
+# remove puncuation
+data = re.sub(r'[^\w\s]', '',data)
+
+# remove numbers
+data = re.sub(r'\d+','',data)
+
+# remove extra space
+data = " ".join(data.split())
+
+
+ # tokkenization
+tokenize_data = word_tokenize(data)
+
+print(tokenize_data)
+
