3143545d5de5e97d15a1952cc209df8e589afc53
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-05-29 17:33:14 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,10 @@
+data ="random 100 words peragaraph which include punchuation symbolsThe old," \
+" dusty bookstore—located on 5th Avenue—was a hidden treasure trove; it smelled of aged paper," \
+" leather, and vanilla. Look at this! exclaimed Sarah, pointing eagerly toward a leather-bound " \
+"journal. The cover was worn, yet beautiful; it had a strange, golden crest engraved in the center." \
+" She asked the clerk, How much for this item? He smiled, stroked his gray beard, and whispered," \
+" For you? It's free. Sarah gasped. Was this a trick? She opened the first page, finding a handwritten note: Adventure awaits"
+
+# print len of data
+print(len(data))
+print(data)
